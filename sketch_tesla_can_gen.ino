db4217037835b5f47ae80271e676e1b406b10043
Sergey <serg_soft@mail.ru>
2019-01-24 11:34:18 +0700

    RPM message gen

---
+++ sketch_tesla_can_gen.ino
@@ -0,0 +1,67 @@
+// demo: CAN-BUS Shield, receive data with check mode
+// send data coming to fast, such as less than 10ms, you can use this way
+// loovee, 2014-6-13
+// sergey, 2018
+
+#include <SPI.h>
+#include "mcp_can.h"
+#define DEBUG_MODE 1
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 10;
+
+char num = 0;
+int index = 0;
+
+
+
+MCP_CAN CAN(SPI_CS_PIN);  // Set CS pin
+
+void setup() {
+  Serial.begin(115200);
+
+START_INIT:
+
+  if (CAN_OK ==
+     // CAN.begin(CAN_500KBPS, MCP_8MHz))  // init can bus : baudrate = 500k
+     CAN.begin(CAN_125KBPS, MCP_8MHz))  // init can bus : baudrate = 500k
+
+  {
+    Serial.println("CAN BUS Shield init ok!");
+  } else {
+    Serial.println("CAN BUS Shield init fail");
+    Serial.println("Init CAN BUS Shield again");
+    delay(100);
+    goto START_INIT;
+  }
+}
+
+void loop() {
+  unsigned char len = 0;
+  unsigned char buf[8];
+
+
+  //send RPM message
+  unsigned char RPM[8] = {0x31, 0xFF,0x2D,0x7F,0xA3,0x14,0x0E,0xA8};
+  CAN.sendMsgBuf(0x105, 0, 8, RPM);
+  delay(50);
+
+  
+  //and generate the load of random messages
+  num = random(10000); //now we test with 2 byte data
+  unsigned char stmp[8] = {'H', 'e', 00 , 01 , 02, ' ', 's', 'e'};
+  // send generated message
+  CAN.sendMsgBuf(num, 0, 8, stmp);
+   delay(50);
+  index = random(8);
+  stmp[index]++;
+  if (stmp[index] > 255) stmp[index] = 0;
+
+
+  
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
